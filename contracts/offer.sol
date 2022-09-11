5a86055bd96d6a02e85cacde2c63330a32535a46
hueso <hueso@torguen.com.ar>
2022-09-11 09:12:37 -0300

    offer cleanup

--- contracts/offer.sol
+++ contracts/offer.sol
@@ -21,33 +21,12 @@ contract Offer {
         owner = payable(msg.sender);
     }
 
-    /* 2. Get message hash to sign
-    getMessageHash(
-        0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C,
-        123,
-        "coffee and donuts",
-        1
-    )
-
-    hash = "0xcf36ac4f97dc10d91fc2cbb20d718e94a8cbfe0f82eaedc6a4aa38946fb797cd"
-    */
     function getMessageHash(
         string memory _message
     ) public pure returns (bytes32) {
         return keccak256(abi.encodePacked( _message));
     }
 
-    /* 3. Sign message hash
-    # using browser
-    account = "copy paste account of signer here"
-    ethereum.request({ method: "personal_sign", params: [account, hash]}).then(console.log)
-
-    # using web3
-    web3.personal.sign(hash, web3.eth.defaultAccount, console.log)
-
-    Signature will be different for different accounts
-    0x993dab3dd91f5c6dc28e17439be475478f5635c92a56e17e82349d3fb2f166196f466c0b4e0c146f285204f0dcb13e5ae67bc33f4b888ec32dfe0a063e8f3f781b
-    */
     function getEthSignedMessageHash(bytes32 _messageHash)
         public
         pure
